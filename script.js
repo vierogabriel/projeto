660e95b59b841ece9b90e6956aff6ef8ae494df8
vierogabriel <163485204+vierogabriel@users.noreply.github.com>
2024-10-26 00:30:41 +0000

    Ç

---
+++ script.js
@@ -0,0 +1,23 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "alternativa 1",
+            "aternativa 2"
+        ]
+    }    
+{
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "alternativa 1",
+            "aternativa 2"
+        ]
+    }
+     "Pergunta 2"
+];
